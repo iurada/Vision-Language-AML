a1fa9b15157b7d29ce02269d72d3663a87b43cfc
Diego <diego.gasco99@gmail.com>
2022-11-23 14:03:58 +0100

    Fix bug in base_model.py line 105

--- models/base_model.py
+++ models/base_model.py
@@ -102,6 +102,6 @@ class DomainDisentangleModel(nn.Module):
         domain_class_dclf = self.domain_classifier(domain_specific) # Minimize cross-entropy loss
         category_class_dclf = self.domain_classifier(category_specific) # Maximize entropy loss
         # Reconstruction process
-        reconstructor = torch.cat(self.reconstructor(domain_specific), self.reconstructor(category_specific))
+        reconstructor = self.reconstructor(torch.cat(domain_specific, category_specific))
         return reconstructor, category_class_cclf, domain_class_cclf, domain_class_dclf, category_class_dclf
 
