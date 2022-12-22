3b854e07b0ff8abeb613042add74337aaf57b466
EnricoMagliano <enrico.magliano@outlook.com>
2022-12-22 17:11:08 +0100

    ziopera

--- experiments/clip_disentangle.py
+++ experiments/clip_disentangle.py
@@ -3,16 +3,16 @@
 class CLIPDisentangleExperiment: # See point 4. of the project
     
     def __init__(self, opt):
-        raise NotImplementedError('[TODO] Implement CLIPDisentangleExperiment.')
+        pass
 
     def save_checkpoint(self, path, iteration, best_accuracy, total_train_loss):
-        raise NotImplementedError('[TODO] Implement CLIPDisentangleExperiment.')
+        pass
 
     def load_checkpoint(self, path):
-        raise NotImplementedError('[TODO] Implement CLIPDisentangleExperiment.')
+        pass
 
     def train_iteration(self, data):
-        raise NotImplementedError('[TODO] Implement CLIPDisentangleExperiment.')
+        pass
 
     def validate(self, loader):
-        raise NotImplementedError('[TODO] Implement CLIPDisentangleExperiment.')
+        pass
