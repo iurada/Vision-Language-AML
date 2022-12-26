3a9dabc9b80d5277dce37852d861a38a6a4958a2
Diego <diego.gasco99@gmail.com>
2022-12-26 10:49:04 +0100

    Bug fix in imports

--- models/base_model.py
+++ models/base_model.py
@@ -6,7 +6,7 @@ from components import *
 class BaselineModel(nn.Module):
     def __init__(self):
         super(BaselineModel, self).__init__()
-        self.feature_extractor = cmp.FeatureExtractor()
+        self.feature_extractor = FeatureExtractor()
         self.category_encoder = nn.Sequential(
             nn.Linear(512, 512),
             nn.BatchNorm1d(512),
@@ -31,29 +31,29 @@ class BaselineModel(nn.Module):
 class DomainDisentangleModel(nn.Module):
     def __init__(self):
         super(DomainDisentangleModel, self).__init__()
-        self.feature_extractor = cmp.FeatureExtractor()
-        self.encoder = cmp.Encoder()
-        self.category_classifier = cmp.CategoryClassifier()
-        self.domain_classifier = cmp.DomainClassifier()
-        self.reconstructor = cmp.Reconstructor()
+        self.feature_extractor = FeatureExtractor()
+        self.encoder = Encoder()
+        self.category_classifier = CategoryClassifier()
+        self.domain_classifier = DomainClassifier()
+        self.reconstructor = Reconstructor()
 
     def forward(self, x, state=None, train=True):
 
         '''Train and validation parts for category encoder's first step'''
         if state == 'category_disentanglement_phase_1' and train == True:
             # Loss for category classifier to minimize
-            cmp.set_requires_grad(self.feature_extractor, requires_grad=True)
-            cmp.set_requires_grad(self.encoder, requires_grad=True)
-            cmp.set_requires_grad(self.category_classifier, requires_grad=True)
+            set_requires_grad(self.feature_extractor, requires_grad=True)
+            set_requires_grad(self.encoder, requires_grad=True)
+            set_requires_grad(self.category_classifier, requires_grad=True)
             x = self.feature_extractor(x)
             x = self.encoder(x, domain=False) # Category encoder
             x = self.category_classifier(x)
             return x
         if state == 'category_disentanglement_phase_1' and train == False:
             # Validation
-            cmp.set_requires_grad(self.feature_extractor, requires_grad=False)
-            cmp.set_requires_grad(self.encoder, requires_grad=False)
-            cmp.set_requires_grad(self.category_classifier, requires_grad=False)
+            set_requires_grad(self.feature_extractor, requires_grad=False)
+            set_requires_grad(self.encoder, requires_grad=False)
+            set_requires_grad(self.category_classifier, requires_grad=False)
             x = self.feature_extractor(x)
             x = self.encoder(x, domain=False) # Category encoder
             x = self.category_classifier(x)
@@ -62,18 +62,18 @@ class DomainDisentangleModel(nn.Module):
         '''Train and validation parts for domain encoder's first step'''
         if state == 'domain_disentanglement_phase_1' and train == True:
             # Loss for domain classifier to minimize
-            cmp.set_requires_grad(self.feature_extractor, requires_grad=True)
-            cmp.set_requires_grad(self.encoder, requires_grad=True)
-            cmp.set_requires_grad(self.domain_classifier, requires_grad=True)
+            set_requires_grad(self.feature_extractor, requires_grad=True)
+            set_requires_grad(self.encoder, requires_grad=True)
+            set_requires_grad(self.domain_classifier, requires_grad=True)
             x = self.feature_extractor(x)
             x = self.encoder(x, domain=True) # Domain encoder
             x = self.domain_classifier(x)
             return x
         if state == 'domain_disentanglement_phase_1' and train == False:
             # Validation
-            cmp.set_requires_grad(self.feature_extractor, requires_grad=False)
-            cmp.set_requires_grad(self.encoder, requires_grad=False)
-            cmp.set_requires_grad(self.domain_classifier, requires_grad=False)
+            set_requires_grad(self.feature_extractor, requires_grad=False)
+            set_requires_grad(self.encoder, requires_grad=False)
+            set_requires_grad(self.domain_classifier, requires_grad=False)
             x = self.feature_extractor(x)
             x = self.encoder(x, domain=True) # Domain encoder
             x = self.domain_classifier(x)
@@ -82,18 +82,18 @@ class DomainDisentangleModel(nn.Module):
         '''Train and validation parts for category encoder's second step'''
         if state == 'category_disentanglement_phase_2' and train == True:
             # Loss for domain classifier to maximize
-            cmp.set_requires_grad(self.feature_extractor, requires_grad=True)
-            cmp.set_requires_grad(self.encoder, requires_grad=True)
-            cmp.set_requires_grad(self.domain_classifier, requires_grad=False)
+            set_requires_grad(self.feature_extractor, requires_grad=True)
+            set_requires_grad(self.encoder, requires_grad=True)
+            set_requires_grad(self.domain_classifier, requires_grad=False)
             x = self.feature_extractor(x)
             x = self.encoder(x, domain=False) # Category encoder
             x = self.domain_classifier(x)
             return x
         if state == 'category_disentanglement_phase_2' and train == False:
             # Validation
-            cmp.set_requires_grad(self.feature_extractor, requires_grad=False)
-            cmp.set_requires_grad(self.disentangler, requires_grad=False)
-            cmp.set_requires_grad(self.domain_classifier, requires_grad=False)
+            set_requires_grad(self.feature_extractor, requires_grad=False)
+            set_requires_grad(self.disentangler, requires_grad=False)
+            set_requires_grad(self.domain_classifier, requires_grad=False)
             x = self.feature_extractor(x)
             x = self.disentangler(x, domain=False) # Category encoder
             x = self.domain_classifier(x)
@@ -102,18 +102,18 @@ class DomainDisentangleModel(nn.Module):
         '''Train and validation parts for domain encoder's second step'''
         if state == 'domain_disentanglement_phase_2' and train == True:
             # Loss for category classifier to maximize
-            cmp.set_requires_grad(self.feature_extractor, requires_grad=True)
-            cmp.set_requires_grad(self.encoder, requires_grad=True)
-            cmp.set_requires_grad(self.category_classifier, requires_grad=False)
+            set_requires_grad(self.feature_extractor, requires_grad=True)
+            set_requires_grad(self.encoder, requires_grad=True)
+            set_requires_grad(self.category_classifier, requires_grad=False)
             x = self.feature_extractor(x)
             x = self.encoder(x, domain=True) # Domain encoder
             x = self.category_classifier(x)
             return x
         if state == 'domain_disentanglement_phase_2' and train == False:
             # Validation
-            cmp.set_requires_grad(self.feature_extractor, requires_grad=False)
-            cmp.set_requires_grad(self.encoder, requires_grad=False)
-            cmp.set_requires_grad(self.category_classifier, requires_grad=False)
+            set_requires_grad(self.feature_extractor, requires_grad=False)
+            set_requires_grad(self.encoder, requires_grad=False)
+            set_requires_grad(self.category_classifier, requires_grad=False)
             x = self.feature_extractor(x)
             x = self.encoder(x, domain=True) # Domain encoder
             x = self.category_classifier(x)
@@ -122,9 +122,9 @@ class DomainDisentangleModel(nn.Module):
         '''Train and validation parts for reconstructor'''
         if state == "feature_reconstruction" and train == True:
             # Loss for reconstructor to minimize
-            cmp.set_requires_grad(self.feature_extractor, requires_grad=True)
-            cmp.set_requires_grad(self.encoder, requires_grad=True)
-            cmp.set_requires_grad(self.reconstructor, requires_grad=True)
+            set_requires_grad(self.feature_extractor, requires_grad=True)
+            set_requires_grad(self.encoder, requires_grad=True)
+            set_requires_grad(self.reconstructor, requires_grad=True)
             x = self.feature_extractor(x)
             fcs = self.encoder(x, domain=False)
             fds = self.encoder(x, domain=True)
@@ -132,9 +132,9 @@ class DomainDisentangleModel(nn.Module):
             return x, rec
         if state == "feature_reconstruction" and train == False:
             # Validation
-            cmp.set_requires_grad(self.feature_extractor, requires_grad=False)
-            cmp.set_requires_grad(self.encoder, requires_grad=False)
-            cmp.set_requires_grad(self.reconstructor, requires_grad=False)
+            set_requires_grad(self.feature_extractor, requires_grad=False)
+            set_requires_grad(self.encoder, requires_grad=False)
+            set_requires_grad(self.reconstructor, requires_grad=False)
             x = self.feature_extractor(x)
             fcs = self.encoder(x, domain=False)
             fds = self.encoder(x, domain=True)
@@ -144,9 +144,9 @@ class DomainDisentangleModel(nn.Module):
         '''Test part'''
         if state == None:
             # Testing part
-            cmp.set_requires_grad(self.feature_extractor, requires_grad=False)
-            cmp.set_requires_grad(self.encoder, requires_grad=False)
-            cmp.set_requires_grad(self.category_classifier, requires_grad=False)
+            set_requires_grad(self.feature_extractor, requires_grad=False)
+            set_requires_grad(self.encoder, requires_grad=False)
+            set_requires_grad(self.category_classifier, requires_grad=False)
             x = self.feature_extractor(x)
             x = self.encoder(x, domain=False)
             x = self.category_classifier(x)
